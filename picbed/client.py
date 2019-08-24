dfc02f408358a27bad517ebde3ce3c937e4cf729
zw <1666013677@qq.com>
2019-08-24 18:32:04 +0800

    11

--- picbed/client.py
+++ picbed/client.py
@@ -1,66 +1,11 @@
 import requests
-import json
-from PIL import ImageGrab, Image
-import clipboard, pickle
-import time, os
-import json
+import os
 
-class Client():
-    """获取token"""
-    def __init__(self):
-        config=self.load('config.json')
-        self.url = config["url"]
-        self.username = config["username"]
-        self.password = config["password"]
-        print(self.url+self.username+self.password)
-        self.headers = {'Content-Type': 'application/json'}
-        self.payload = {'username':self.username, 'password':self.password}
-        self.pkiFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'token.pki')
-    
-    def load(self,fname):
-        filename=fname   #读取json文件中的某个数值
-        with open(filename,encoding='utf-8') as f:
-            pop_data=json.load(f)
-            return pop_data
-    
-    def getToken(self):
-        # 获取token
-        token = {}
-        r = requests.post(self.url,headers=self.headers, data=json.dumps(self.payload))
-        t = r.text 
-        timestamp = int(time.time())
-        token.setdefault('token', t)
-        token.setdefault('timestamp', timestamp)
-        with open(self.pkiFile, 'wb') as f:
-            pickle.dump(token,f)
-
-    def showToken(self):
-        # 从文件获取token
-        with open(self.pkiFile, 'rb') as f:
-            t = pickle.load(f)
-            return t.get('token')
-
-    def tokenExpired(self):
-        # 验证是否过期
-        if not os.path.exists(self.pkiFile):
-            self.getToken()
-        with open(self.pkiFile, 'rb') as f:
-            token = pickle.load(f)
-        if int(time.time()) - token.get('timestamp') > 600:
-            return True
-        else:
-            return False
-
-
-    def upload(self):
-        upload_url = self.url + 'upload'
-        #files = {'image001': open(r'001.jpg', 'rb')}
-        files= {"image001":open(os.sep.join(['','001.jpg']), "rb")}
-        h1 = {'token':token}
-        ru = requests.post(upload_url, files=files)
-        print(ru.text)
+def upload():
+    upload_url = 'http://127.0.0.1:10000/upload'
+    files= {"image001":open('1.png', "rb")}
+    ru = requests.post(upload_url, files=files)
+    print(ru.text)
 
 if __name__ == '__main__':
-    c = Client()
-    token = c.showToken()
-    c.upload()
+    upload()
--- picbed/server.py
+++ picbed/server.py
@@ -1,32 +1,10 @@
 # coding:utf-8
-from flask import Flask, request, jsonify, url_for
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.utils import secure_filename
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
+from flask import Flask, request, jsonify,url_for
 import os, time
 import hashlib
-from flask import url_for
-
-from flask import Flask, render_template, jsonify, request, make_response, send_from_directory, abort
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = 'guess me'
-app.config['JSON_AS_ASCII'] = False
 app.config['UPLOAD_FOLDER'] = 'static/'
-app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
-db = SQLAlchemy(app)
-
-msg_default = {
-    "msg": "invalid args"
-}
-
-def allow_file(filename):
-        return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 def gen_filename(filename):
@@ -34,66 +12,6 @@ def gen_filename(filename):
     timestamp = time.time()
     tmp_str = '{}{}'.format(timestamp, filename)
     return (hashlib.md5(tmp_str.encode('utf-8')).hexdigest()) + "." +  file_format
-
-
-
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), nullable=False, unique=True)
-    password_hash = db.Column(db.String(200))
-
-    def gen_auth_token(self, expiration=600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            # token过期
-            return None
-        except BadSignature:
-            # token无效
-            return None
-        user = User.query.get(data['id'])
-        return user
-
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-
-    def verify_password(self, password):
-        # 验证密码
-        return check_password_hash(self.password_hash, password)
-
-    @password.setter
-    def password(self, password):
-        # 生成密码
-        self.password_hash = generate_password_hash(password)
-
-# 获取token
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        #username = data.get('username', None)
-        #password = data.get('password', None)
-        '''if username and password:
-            user = User.query.filter_by(username=username).first()
-            if user is not None and user.verify_password(password):
-                token = user.gen_auth_token()
-                return token
-            else:
-                return jsonify(msg_default)
-        else:
-            return jsonify(msg_default)
-    else: 
-        return jsonify(msg_default)
-'''
 
 # 上传图片
 @app.route('/upload', methods=['POST'])
@@ -110,21 +28,6 @@ def upload():
             upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
             ext_link = url_for('static', filename=filename,  _external=True, _scheme='http')
             return ext_link
-        '''token = request.headers.get('token')
-        u1 = User.verify_auth_token(token)
-        if u1 is not None and u1 is not False:
-            print("token有效")
-            if upload_file:
-                filename = gen_filename(upload_file.filename)
-                print("获取到filename {}".format(filename))
-                upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
-                ext_link = url_for('static', filename=filename,  _external=True, _scheme='https')
-                return ext_link
-        else:
-            msg = {"msg":"invalid token"}
-            return jsonify(msg)
-    else:
-        return jsonify(msg_default)'''
 
 @app.route('/static/<string:filename>', methods=['GET'])
 def show_photo(filename):
@@ -140,9 +43,6 @@ def show_photo(filename):
     else:
         pass
 
-
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=18800, debug=True)
+    app.run(host='127.0.0.1',port=10000, debug=True)
 
